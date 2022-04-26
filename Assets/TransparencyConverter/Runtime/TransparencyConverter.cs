08009dc261df24326df6ff0203183d618eca5302
sotanmochi <16936357+sotanmochi@users.noreply.github.com>
2022-04-27 02:39:23 +0900

    Fix a bug

--- Assets/TransparencyConverter/Runtime/TransparencyConverter.cs
+++ Assets/TransparencyConverter/Runtime/TransparencyConverter.cs
@@ -18,17 +18,23 @@ namespace TransparencyConverter
 
             var color_srgb = foregroundColor * alpha_srgb + backgroundColor * (1 - alpha_srgb);
 
-            var cf = foregroundColor.X;
-            var cb = backgroundColor.X;
-            var c  = color_srgb.X;
+            var cf = 0f;
+            var cb = 0f;
+            var c  = 0f;
 
-            if (color_srgb.Y > color_srgb.X && color_srgb.Y > color_srgb.Z)
+            if (diff.X != 0)
+            {
+                cf = foregroundColor.X;
+                cb = backgroundColor.X;
+                c  = color_srgb.X;
+            }
+            else if (diff.Y != 0)
             {
                 cf = foregroundColor.Y;
                 cb = backgroundColor.Y;
                 c  = color_srgb.Y;
             }
-            else if (color_srgb.Z > color_srgb.X && color_srgb.Z > color_srgb.Y)
+            else if (diff.Z != 0)
             {
                 cf = foregroundColor.Z;
                 cb = backgroundColor.Z;
